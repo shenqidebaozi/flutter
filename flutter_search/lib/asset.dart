8536c61c7182b2d6dfc298021fc4e81772bf80dd
shenqidebaozi <906604588@qq.com>
2019-05-03 15:16:30 +0800

    搜索

---
+++ flutter_search/lib/asset.dart
@@ -0,0 +1,2 @@
+const searchList = ["jiejie-大长腿", "jiejie-水蛇腰", "gege-帅气欧巴", "gege-小鲜肉"];
+const recentSuggest = ["推荐1", "推荐2"];
---
+++ flutter_search/lib/search_bar_demo.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'asset.dart';
+
+class SearchBarDemo extends StatefulWidget {
+  @override
+  _SearchBarDemoState createState() => _SearchBarDemoState();
+}
+
+class _SearchBarDemoState extends State<SearchBarDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search bar demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: searchBarDelegate());
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class searchBarDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () => query = "",
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return Container(
+      width: 100,
+      height: 100,
+      child: Card(
+        color: Colors.redAccent,
+        child: Center(
+          child: Text(query),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentSuggest
+        : searchList.where((input) => input.startsWith(query)).toList();
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) => ListTile(
+              title: RichText(
+                  text: TextSpan(
+                      text: suggestionList[index].substring(0, query.length),
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                      children: [
+                    TextSpan(
+                        text: suggestionList[index].substring(query.length),
+                        style: TextStyle(color: Colors.grey))
+                  ])),
+            ));
+  }
+}
